bbd1eba2e725f9be0a0cdd1d6b3b3b576a9a6364
Ankita Chandra <ankita.chandra@kiwitech.com>
2022-09-09 14:23:34 +0530

    swaggor issue resolve

--- app/Services/DonarDashboardService.php
+++ app/Services/DonarDashboardService.php
@@ -10,7 +10,7 @@ class DonarDashboardService
     public function getDonarProfileCard()
     {
         return User::select(ID,FIRST_NAME,MIDDLE_NAME,LAST_NAME,ROLE_ID,COUNTRY_CODE,PHONE_NO,EMAIL, PROFILE_PIC, RECENT_ACTIVITY)->where(ROLE_ID, PARENTS_TO_BE)
-        ->selectRaw('DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),dob)), "%Y")+0 AS age');
+        ->selectRaw('DATE_FORMAT(FROM_DAYS(DATEDIFF(now(),dob)), "%Y")+0 AS age')
         ->with(['user_profile' => function($q) {
             return $q->select(['id','user_id','dob','gender_id','sexual_orientations_id','relationship_status_id','occupation','bio'
             ]);
