3abe8141679f70de195e53f5d186bbab516e396b
rajan singh <rajan.singh@kiwitech.com>
2022-09-21 13:17:38 +0530

    MBCBCK-44 | send notification

--- app/Jobs/SubscriptionWillEnd.php
+++ app/Jobs/SubscriptionWillEnd.php
@@ -9,10 +9,14 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use App\Helpers\CustomHelper;
+use App\Models\Notification;
+use App\Models\User;
+use App\Models\DeviceRegistration;
+use App\Notifications\SubscriptionReminder;
 
 class SubscriptionWillEnd implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, FcmTrait;
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     protected $data;
 
@@ -34,18 +38,29 @@ class SubscriptionWillEnd implements ShouldQueue
     public function handle()
     {
         $subscriptionEndDate = CustomHelper::dateConvert($this->data->current_period_end, YMD_FORMAT);
-        $this->saveNotification($subscriptionEndDate, $this->data->user_id);
-        $membershipArrayPtb[USER_ID] = $this->data->user_id;
+        $title = 'Renew Susbcription!';
+        $description  = 'Your subscription will end on '.$subscriptionEndDate.'. Please renew to get good matches.';
+        $userId = $this->data->user_id;
+        $this->saveNotificationInDB($title, $description, $userId);
+        $membershipArrayPtb[USER_ID] = $userId;
         $membershipArrayPtb[NAME] = $this->data->user->first_name.' '.$this->data->user->last_name;
         $membershipArrayPtb['membership_name'] = $this->data->subscriptionPlan->name;
         $membershipArrayPtb['membership_end'] = $subscriptionEndDate;
         $membershipArrayPtb['membership_id'] = $this->data->id;
+        $userDevice = DeviceRegistration::where([USER_ID => $userId, STATUS => ACTIVE])->first();
+        if ($userDevice != null) {
+            $ptb = User::find($userId);
+            $ptb->notify(new SubscriptionReminder($userDevice->device_token, $title, $description, $MembershipArray));
+        }
     }
 
-    private function saveNotification($endDate, $recipient) {
-        $title = 'Renew Susbcription!';
-        $description  = 'Your subscription will end on '.$endDate.'. Please renew to get good matches.';
-        FcmTrait::saveNotificationInDB($title, $description, $recipient);
+    private function saveNotificationInDB($title, $description, $recipient) {
+        Notification::create([
+            'title' => $title,
+            'description' => $description,
+            'recipient' => $recipient,
+        ]);
+
         return true;
     }
 }
