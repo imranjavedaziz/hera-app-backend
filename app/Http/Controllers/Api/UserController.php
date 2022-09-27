9618c4561952a45b0e50696e52143959eff341e6
Ankita Chandra <ankita.chandra@kiwitech.com>
2022-09-27 13:47:35 +0530

    changes in request

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -123,7 +123,7 @@ class UserController extends Controller
      *      ),
      *  )
      */
-    public function register(Request $request)
+    public function register(RegisterRequest $request)
     {
         try {
             DB::beginTransaction();
