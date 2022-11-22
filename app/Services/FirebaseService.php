e0804b739aee348879987d5ad9fe792fdbd36de3
rajan singh <rajan.singh@kiwitech.com>
2022-11-22 18:05:24 +0530

    Add chat thred for donar request

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -240,6 +240,11 @@ class FirebaseService
                 $this->database->getReference($this->tableName.'/'.$user1->id.'/'.$this->friendsKey.'/'.$user2->id.'/'.MATCH_REQUEST)->update([FROM_USER_ID => $user1->id, TO_USER_ID => $user2->id,STATUS => TWO]);
             } else {
                 $chatUser1FriendData = $this->createFriend($user1,$user2);
+                /***Add User 1 Friends ***/
+                if ($this->database->getReference($this->tableName)->getSnapshot()->hasChild($user1->id.'/'.$this->friendsKey.'/'.$user2->id) === false){
+                    $this->database->getReference($this->tableName)->update([$user1->id.'/'.$this->friendsKey.'/'.$user2->id => '']);
+                    $response = $this->database->getReference($this->tableName.'/'.$user1->id.'/'.$this->friendsKey.'/'.$user2->id)->set($chatUser1FriendData);
+                }
             }
             /***Update User 2 Friends ***/
             if ($this->database->getReference($this->tableName)->getSnapshot()->hasChild($user2->id.'/'.$this->friendsKey.'/'.$user1->id) === true){
@@ -247,6 +252,10 @@ class FirebaseService
                 $this->database->getReference($this->tableName.'/'.$user2->id.'/'.$this->friendsKey.'/'.$user1->id.'/'.MATCH_REQUEST)->update([FROM_USER_ID => $user1->id, TO_USER_ID => $user2->id, STATUS => TWO]);
             } else {
                 $chatUser2FriendData = $this->createFriend($user2,$user1);
+                if ($this->database->getReference($this->tableName)->getSnapshot()->hasChild($user2->id.'/'.$this->friendsKey.'/'.$user1->id) === false){
+                    $this->database->getReference($this->tableName)->update([$user2->id.'/'.$this->friendsKey.'/'.$user1->id => '']);
+                    $response = $this->database->getReference($this->tableName.'/'.$user2->id.'/'.$this->friendsKey.'/'.$user1->id)->set($chatUser2FriendData);
+                }
             }
        } catch (ApiException $e) {
             $request = $e->getRequest();
