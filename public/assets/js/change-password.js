08cc59b15959fcd0e2e70f6440363949d69a581d
Ankita Chandra <ankita.chandra@kiwitech.com>
2022-11-28 16:22:00 +0530

    update in design inquiry export add design change-password

--- public/assets/js/change-password.js
+++ public/assets/js/change-password.js
@@ -43,47 +43,8 @@ $(function () {
                 },
                 success: function (data) {
                   console.log(data);
-                    // $('#first-name-error').empty()
-                    // $('#last-name-error').empty()
-                    // $('#gender-error').empty()
-                    // $('#email-error').empty()
-                    // $('#phone-error').empty()
-                    // $('#password-error').empty()
-                    // $('#captcha-error').empty()
                     if (data.errors) {
                       console.log(data.errors)
-                    //     // $('#phone-number').attr('disabled', true);
-                    //     if (data.errors.first_name) {
-                    //         $('#first-name-error').html(data.errors.first_name[0]);
-                    //     }
-                    //     if (data.errors.last_name) {
-                    //         $('#last-name-error').html(data.errors.last_name[0]);
-                    //     }
-                    //     if (data.errors.gender) {
-                    //         $('#gender-error').html(data.errors.gender[0]);
-                    //     }
-                    //     if (data.errors.email) {
-                    //         $('#email-error').html(data.errors.email[0]);
-                    //     }
-                    //     if (data.errors.phone) {
-                    //         $('#phone-error').html(data.errors.phone[0]);
-                    //     }
-                    //     if (data.errors.password) {
-                    //         $('#password-error').html(data.errors.password[0]);
-                    //     }
-
-                    //     var captcha = "g-recaptcha-response";
-                    //     if (data.errors[captcha]) {
-                    //         $('#captcha-error').html(data.errors[captcha][0]);
-                    //     }
-                    }
-                    // if (data.success) {
-                    //     $('#registerForm')[0].reset();
-                    //     $('.error-response').empty();
-                    //     $('.success-response').html("@lang('labels.frontend.modal.registration_message')");
-                    //     alert("@lang('labels.frontend.modal.registration_message')");
-                    //     location.href = "{{ route('frontend.auth.select-login') }}";
-                    // }
                 },
                 error: function(XMLHttpRequest, errorThrown) {
                     console.log(XMLHttpRequest);
