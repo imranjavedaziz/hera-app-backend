cf872a37936e5d1e3888e3285463f0f9685b487b
Ankita Chandra <ankita.chandra@kiwitech.com>
2022-12-02 18:08:22 +0530

    bug remove

--- app/Traits/SetterDataTrait.php
+++ app/Traits/SetterDataTrait.php
@@ -75,19 +75,19 @@ trait setterDataTrait {
     private function setUserName($role_id, $user_id){
         switch ($role_id) {
             case 2:
-                $username = 'PTB'.random_int(1000, 9999).$user_id;
+                $username = 'PTB'.rand(1000, 9999).$user_id;
                 break;
             case 3:
-                $username = 'SM'.random_int(1000, 9999).$user_id;
+                $username = 'SM'.rand(1000, 9999).$user_id;
                 break;
             case 4:
-                $username = 'ED'.random_int(1000, 9999).$user_id;
+                $username = 'ED'.rand(1000, 9999).$user_id;
                 break;
             case 5:
-                $username = 'SD'.random_int(1000, 9999).$user_id;
+                $username = 'SD'.rand(1000, 9999).$user_id;
                 break;
             default:
-            $username = 'MBC'.random_int(1000, 9999).$user_id;
+            $username = 'MBC'.rand(1000, 9999).$user_id;
                 break;
         }
         return $username;
