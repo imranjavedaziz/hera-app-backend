fa4c6409131017ae70be41caf57c3badb64fbab2
Ankita Chandra <ankita.chandra@kiwitech.com>
2022-12-06 13:23:22 +0530

    required changes

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -95,8 +95,8 @@ return [
     'password_matched' => 'Password matched successfully.',
     'account_delete_success' => 'Your account has been deleted successfully!',
     'user_account_deleted' => 'Your account has been deleted and no longer available.',
-    'user_account_deactivated_by_admin' => 'Your account has been deactivated by admin. Please contact admin to get it reactivated.',
-    'user_account_deleted_by_admin' => 'Your account has been deleted by admin. Please contact HERA support',
+    'user_account_deactivated_by_admin' => 'Your account has been deactivated by admin. Please contact HERA support.',
+    'user_account_deleted_by_admin' => 'Your account has been deleted by admin. Please contact HERA support.',
     'admin' => [
         'invalid_credentail' => 'Please login with valid credentials.',
         'account_deactive' => 'User has been deactivated.',
