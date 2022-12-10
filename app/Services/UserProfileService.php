5b528e5ab194b904e868386f7988c3a68860bf33
Ankita Chandra <ankita.chandra@kiwitech.com>
2022-12-10 16:45:44 +0530

    update api of details profiles

--- app/Services/UserProfileService.php
+++ app/Services/UserProfileService.php
@@ -33,7 +33,7 @@ class UserProfileService
                 ->selectRaw('(select name from education where id='.EDUCATION_ID.AS_CONNECT.EDUCATION.' ');
             }, LOCATION, DONERPHOTOGALLERY, DONERVIDEOGALLERY
         ])->where(ID, $input[USER_ID])->first();
-        $input[RECEIVER_ID] = AuthHelper::authenticatedUser()->id; //use sender id  as reciver_id for matching chat section
+        $input[RECEIVER_ID] = AuthHelper::authenticatedUser()->id; /** use sender id  as reciver_id for matching chat section **/
         $input[MESSAGE] = "";
         $user->profile_match_request = $this->profileMatchRequest($input[RECEIVER_ID], $input[USER_ID]);
         $user->profile_match_chat = FcmService::sendPushNotification($input, $input[USER_ID],false);
