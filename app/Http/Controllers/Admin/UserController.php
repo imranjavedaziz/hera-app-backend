039af46101d02dc44446129d45a9e8f4ff340bd2
KiwiTech <deepanshu.gogia@kiwitech.com>
2023-04-20 16:36:58 +0530

    adding file size 5 mb limit

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -135,7 +135,7 @@ class UserController extends AdminController
         try {
             // Validate the uploaded file
             $validator = Validator::make($request->all(), [
-                'file' => 'required|mimes:csv,xlsx'
+                'file' => 'required|mimes:csv,xlsx|max:5120'//5 mb maximum file size
             ]);
 
             if ($validator->fails()) {
